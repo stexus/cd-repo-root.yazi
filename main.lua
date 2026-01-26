d9e7a5dd3ab92f137533f76b70ac2bfa80702a87
Massimo Tseng <massimo.tseng@gmail.com>
2026-01-25 16:03:53 -0800

    support sapling repos

--- main.lua
+++ main.lua
@@ -1,12 +1,9 @@
 --- @since 25.5.31
-
-local function get_git_toplevel()
-	local command = "git rev-parse --show-toplevel 2>&1"
+local function run_command(command)
 	local handle = io.popen(command)
 	local result = handle:read("*a")
 	local status_table = { handle:close() }
 	local status_code = status_table[3]
-
 	if status_code == 0 then
 		local destination = result:gsub("[\n\r]", "") .. "/"
 		return destination
@@ -15,9 +12,21 @@ local function get_git_toplevel()
 	end
 end
 
+local function get_repo_toplevel()
+	local destination = run_command("sl root 2>&1")
+	if destination then
+		return destination
+	end
+	destination = run_command("git rev-parse --show-toplevel 2>&1")
+	if destination then
+		return destination
+	end
+	return nil
+end
+
 return {
 	entry = function()
-		local destination = get_git_toplevel()
+		local destination = get_repo_toplevel()
 		ya.dbg(destination)
 		if destination then
 			local target = Url(destination)
@@ -25,7 +34,7 @@ return {
 		else
 			ya.notify({
 				title = "Could not change directory!",
-				content = "You are not in a git repository.",
+				content = "You are not in a git or sapling repository.",
 				timeout = 3,
 				level = "error",
 			})
