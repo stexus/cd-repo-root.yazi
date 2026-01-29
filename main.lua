b464268cb6f4434120bca065da42ca04244ed413
Massimo Tseng <massimo.tseng@gmmail.com>
2026-01-29 14:11:04 -0800

    use pure lua to walk directory up to find .hg

--- main.lua
+++ main.lua
@@ -1,36 +1,39 @@
 --- @since 25.5.31
-local function run_command(command)
-	local handle = io.popen(command)
-	local result = handle:read("*a")
-	local status_table = { handle:close() }
-	local status_code = status_table[3]
-	if status_code == 0 then
-		local destination = result:gsub("[\n\r]", "") .. "/"
-		return destination
-	else
-		return nil
+local get_cwd = ya.sync(function()
+	return tostring(cx.active.current.cwd)
+end)
+
+local function dir_exists(path)
+	local f = io.open(path .. "/.", "r")
+	if f then
+		f:close()
+		return true
 	end
+	return false
 end
 
 local function get_repo_toplevel()
-	local destination = run_command("sl root 2>&1")
-	if destination then
-		return destination
+	local path = get_cwd()
+
+	while path and path ~= "" do
+		if dir_exists(path .. "/.hg") or dir_exists(path .. "/.git") then
+			return path .. "/"
+		end
+		local parent = path:match("(.+)/[^/]+$")
+		if not parent then
+			break
+		end
+		path = parent
 	end
-	destination = run_command("git rev-parse --show-toplevel 2>&1")
-	if destination then
-		return destination
-	end
+
 	return nil
 end
 
 return {
 	entry = function()
 		local destination = get_repo_toplevel()
-		ya.dbg(destination)
 		if destination then
-			local target = Url(destination)
-			ya.emit("cd", { target })
+			ya.emit("cd", { Url(destination) })
 		else
 			ya.notify({
 				title = "Could not change directory!",
